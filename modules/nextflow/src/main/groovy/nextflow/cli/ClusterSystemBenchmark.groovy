89f0c7730ac68851db53c374cee749bcf32c8dc6
FredericRisling <frisling@live.de>
2021-12-26 19:42:58 +0100

    add cluster gflop benchmarking

--- modules/nextflow/src/main/groovy/nextflow/cli/ClusterSystemBenchmark.groovy
+++ modules/nextflow/src/main/groovy/nextflow/cli/ClusterSystemBenchmark.groovy
@@ -4,6 +4,8 @@ import groovy.util.logging.Slf4j
 import nextflow.k8s.client.K8sClient
 import nextflow.k8s.client.K8sResponseJson
 
+import java.security.SecureRandom
+
 @Slf4j
 class ClusterSystemBenchmark implements SystemBenchmark{
     private K8sClient api
@@ -15,15 +17,19 @@ class ClusterSystemBenchmark implements SystemBenchmark{
     @Override
     void renderHardwareDocument() {
 
-        log.info("render Cluster Hardware")
+        log.info("Benchmarking Cluster Hardware...")
         List<K8sNode> nodes = getNodes()
+        nodes.forEach(it->log.info("$it.name: $it.roles"))
 
 
         for(node in nodes){
-            log.info(node.toString())
-//            node.gFlops = benchmarkGFlops(node)
+            submitGFlopsPod(node)
+            log.info("Benchmarking node: " + node.name + " ...")
+            Double gFlops = parseGFlopsForNode(node)
+            node.setgFlops(gFlops)
 //            node.cores = getCores(node)
 //            node.disk = getDisk(node)
+            log.info(node.toString())
         }
 //        List<String> networkLink = benchmarkNetworkLink()
     }
@@ -43,36 +49,6 @@ class ClusterSystemBenchmark implements SystemBenchmark{
         nodes
     }
 
-    List<K8sNode.Roles> getK8sRolesFromString(String str){
-        ArrayList<K8sNode.Roles> ret = new ArrayList<>()
-        if(str.contains("master")) ret.add(K8sNode.Roles.MASTER)
-        if(str.contains("control-plane")) ret.add(K8sNode.Roles.CONTROL_PLANE)
-        else if(str.contains("worker")) ret.add(K8sNode.Roles.WORKER)
-        else ret.add(K8sNode.Roles.NONE)
-        return ret
-    }
-
-    Double benchmarkGFlops(K8sNode node){
-        //TODO
-        return null
-    }
-
-
-    void renderForClusterHardware(){
-        log.info("RenderForClusterHardware invoked")
-
-        //get all nodes
-//        ArrayList<String> resp = executeCommand([ "kubectl", "get", "nodes"])
-//        resp.forEach(it -> log.info(it))
-//        ArrayList<String> splitted = resp.first().split(" ")
-//        log.info("size after filtering: " + splitted.stream().filter(str -> str!= "").count())
-//
-        def response = createSingleContainerPod("benchmark-34", "default", \
- "example34", "arrayfire/arrayfire", ["/bin/bash"] as String[])
-        log.info("RESPONSE:" + response.toString())
-
-    }
-
     static List<String> executeCommand(List<String> command){
         ProcessBuilder processBuilder = new ProcessBuilder(command)
         processBuilder.directory(new File(System.getProperty("user.home")))
@@ -90,48 +66,105 @@ class ClusterSystemBenchmark implements SystemBenchmark{
         }
     }
 
-    K8sResponseJson createSingleContainerPod(String name, String namespace, String containerName,  \
-  String containerImage, String[] command){
+    List<K8sNode.Roles> getK8sRolesFromString(String str){
+        ArrayList<K8sNode.Roles> ret = new ArrayList<>()
+        if(str.contains("master")) ret.add(K8sNode.Roles.MASTER)
+        if(str.contains("control-plane")) ret.add(K8sNode.Roles.CONTROL_PLANE)
+        else if(str.contains("worker")) ret.add(K8sNode.Roles.WORKER)
+        else ret.add(K8sNode.Roles.NONE)
+        return ret
+    }
 
-        LinkedHashMap<String, Serializable> request = new LinkedHashMap<>()
-        request.put("apiVersion", "v1")
-        request.put("kind", "Pod")
+    void submitGFlopsPod(K8sNode node){
+        Map<String, Serializable> request = new LinkedHashMap<>()
+        String apiVersion = "v1"
+        String kind = "Pod"
+        //metaData
+        String name = "benchmarkgflops-"+ node.name
+        String namespace = "default"
+        request.putAll(makePodHead(apiVersion, kind, name, namespace))
 
-        //meta data
+        //create spec
+        //create container
+        Map<String, Serializable> container = defineContainer("/bin/sh", ["-c", "cd build ; cd examples; cd benchmarks ; ./blas_cpu"],
+            "arrayfire/arrayfire", "arrayfire-"+node.name, "5722Mi", 2)
+        ArrayList<Map<String, Serializable>> containerList = new ArrayList<>()
+        containerList.add(container)
+
+        Map<String, Serializable> spec = new LinkedHashMap<>()
+        spec.put("containers", containerList)
+        spec.put("restartPolicy", "Never")
+        spec.put("nodeName", node.name)
+
+        //add spec to req
+        request.put("spec", spec)
+
+        //create pod
+        api.podCreate(request, null)
+    }
+
+    Double parseGFlopsForNode(K8sNode node){
+        Double gFlops
+        String gFlopsPod = "benchmarkgflops-"+ node.name
+        while(!executeCommand(["kubectl", "get", "pod", gFlopsPod]).get(1).contains("Completed")){
+            Thread.sleep(10000)
+        }
+        try{
+            List<String> reads = executeCommand(["kubectl", "logs", gFlopsPod])
+            String peak = reads.last()
+            String flops = peak.split(" ")[3]
+            gFlops = Double.parseDouble(flops)
+        }
+        catch (Exception e){
+            e.printStackTrace()
+            log.warn("WARN: Could not benchmark GFlops for node: $node.name; \n" +
+                    " Possible causes: node unavailable or memory/cpu unsifficient")
+        }
+        gFlops
+    }
+
+    Map<String, Serializable> makePodHead(String apiVersion, String kind, String name, String namespace){
+        LinkedHashMap<String, Serializable> ret = new LinkedHashMap<>()
+        ret.put("apiVersion", apiVersion)
+        ret.put("kind", kind)
+        //metaData
         LinkedHashMap<String, Serializable> metaData = new LinkedHashMap<>()
         metaData.put("name", name)
         metaData.put("namespace", namespace)
-        //meta data labels
-        LinkedHashMap<String, Serializable> metaDataLabels = new LinkedHashMap<>()
-        metaDataLabels.put("app", "nextflow")
-        metaDataLabels.put("runName", name)
-
-        metaData.put("labels", metaDataLabels)
-
-        request.put("metadata", metaData)
-
-        //spec
-        LinkedHashMap<String, Serializable> spec = new LinkedHashMap<>()
-        //containers
-        LinkedHashMap<String, Serializable> containers = new LinkedHashMap<>()
-        containers.put("name", containerName)
-        containers.put("image", containerImage)
-
-        ArrayList<String> commandContainer = new ArrayList<>()
-        for(entry in command){
-            commandContainer.add(entry)
-        }
-        containers.put("command", commandContainer)
-
-        ArrayList<LinkedHashMap> containerList = new ArrayList<>()
-        containerList.add(containers)
-        spec.put("containers", containerList)
-
-        request.put("spec", spec)
-
-        log.info(request.toString())
-        return api.podCreate(request, null)
+        //add metaData to Head
+        ret.put("metadata", metaData)
+        //return full pod head for yaml file
+        ret
     }
+
+    Map<String, Serializable> defineContainer(String command, List<String> args, String image, \
+        String containerName, String memory, int cpu){
+
+        LinkedHashMap<String, Serializable> containerProperties = new LinkedHashMap<>()
+        List<String> commandAsList = new ArrayList<>()
+        commandAsList.add(command)
+        containerProperties.put("command", commandAsList)
+        containerProperties.put("args", args)
+        containerProperties.put("image", image)
+        containerProperties.put("name", containerName)
+
+        //mem and cpu
+        LinkedHashMap<String, Serializable> memAndCpu = new LinkedHashMap<>()
+        memAndCpu.put("memory", memory)
+        memAndCpu.put("cpu", cpu)
+        //resources limits
+        LinkedHashMap<String, Serializable> limit = new LinkedHashMap<>()
+        limit.put("limits", memAndCpu)
+        //resources requests
+        LinkedHashMap<String, Serializable> request = new LinkedHashMap<>()
+        request.put("requests", memAndCpu)
+
+        limit.putAll(request)
+        containerProperties.put("resources", limit)
+
+        containerProperties
+    }
+
 
     class K8sNode{
         String name
