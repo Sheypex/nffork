5adcab8178367ace2c43d83328c6f9d5c436ef57
Alexander Groth <sheypex@gmail.com>
2023-05-12 14:07:12 +0200

    built

--- modules/nf-commons/src/main/nextflow/Const.groovy
+++ modules/nf-commons/src/main/nextflow/Const.groovy
@@ -58,12 +58,12 @@ class Const {
     /**
      * The app build time as linux/unix timestamp
      */
-    static public final long APP_TIMESTAMP = 1681553308395
+    static public final long APP_TIMESTAMP = 1683893140029
 
     /**
      * The app build number
      */
-    static public final int APP_BUILDNUM = 5860
+    static public final int APP_BUILDNUM = 5861
 
     /**
      * The app build time string relative to UTC timezone
